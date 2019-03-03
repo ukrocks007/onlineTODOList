8c6ea787c4b446cb19162445c4139b8963fe127d
Utkarsh Mehta <utkarsh.mehta678@live.com>
2019-03-03 14:27:58 +0530

    Fixed the backend config

--- backend/configs/config.js
+++ backend/configs/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    MONGO_URL: "mongodb://mongo:27017/ToDoList" || process.env.MONGO_URL,
+    MONGO_URL: "mongodb://localhost:27017/ToDoList" || process.env.MONGO_URL,
+    //Use "mongodb://mongo:27017/ToDoList" when using docker
     JWT: {
         secret: "secretTODOList@123" || process.env.JWTSecret,
         expire: 604800
