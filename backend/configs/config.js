a66c02513e58716932658a3f330c78c8308f4f11
Utkarsh Mehta <utkarsh.mehta678@live.com>
2019-03-03 14:12:22 +0530

    Added docker & docker compose file

--- backend/configs/config.js
+++ backend/configs/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGO_URL: "mongodb://localhost:27017/ToDoList" || process.env.MONGO_URL,
+    MONGO_URL: "mongodb://mongo:27017/ToDoList" || process.env.MONGO_URL,
     JWT: {
         secret: "secretTODOList@123" || process.env.JWTSecret,
         expire: 604800
